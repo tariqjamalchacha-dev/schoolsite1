fc962710bf76ecb7a82826ba0e58ed098bc299fd
tariq jameel <tariqjamalchacha@gmail.com>
2025-10-30 05:33:37 +0000

    make the gap to double current of hero buttons and also remove the half

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
             priority
           />
         )}
-        <div className="relative z-10 flex gap-16">
+        <div className="relative z-10 flex gap-32">
             <Button asChild size="lg">
                 <Link href="/calendar">View Calendar</Link>
             </Button>
